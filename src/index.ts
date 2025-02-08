51decb7feda509cf7ac48444af8536fdba68dbfe
AJ Milbauer <ajmilbauer@AJs-MacBook-Pro.local>
2025-02-08 16:50:08 -0500

    updating allowed origins

--- src/index.ts
+++ src/index.ts
@@ -9,11 +9,21 @@ const port = process.env.PORT || 3000;
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 
-app.use((_, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+app.use((req, res, next) => {
+  const allowedOrigins = [
+    "http://localhost:5173",
+    "https://cozy-threads-app.onrender.com",
+  ];
+
+  const origin = req.headers.origin;
+  if (origin && allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   res.setHeader("Access-Control-Allow-Credentials", "true");
+
   next();
 });
 
